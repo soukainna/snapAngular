f0c75ea4fc588e5d8e03e10bbcbe14f5f2134e4f
soukainna <soukaina1lafdili@gmail.com>
2022-07-18 15:43:22 +0200

    add methode oninit

--- snap-face/src/app/face-snap/face-snap.component.ts
+++ snap-face/src/app/face-snap/face-snap.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./face-snap.component.css']
 })
 export class FaceSnapComponent implements OnInit {
-
+  title!: string;
+  description!: string;
+  date!: Date;
+  snap!: number;
+  imgUrl!: string;
   constructor() { }
 
   ngOnInit(): void {
+    this.title = 'Bulbizarre';
+    this.imgUrl = 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png'
+    this.description = 'Bulbizarre est une espèce de Pokémon de type Herbe/Poison !';
+    this.date = new Date();
+    this.snap = 6;
   }
 
 }
