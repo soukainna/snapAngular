547f61e7eb865f97afef08bf72b8d5157670f250
soukainna <soukaina1lafdili@gmail.com>
2022-07-22 17:52:39 +0200

    add Local id for location

--- snap-face/src/app/app.module.ts
+++ snap-face/src/app/app.module.ts
@@ -1,10 +1,15 @@
 import { DatePipe } from '@angular/common';
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FaceSnapComponent } from './face-snap/face-snap.component';
+
+//add location france
+
+import { registerLocaleData } from '@angular/common';
+import * as fr from '@angular/common/locales/fr';
 
 @NgModule({
   declarations: [
@@ -15,7 +20,13 @@ import { FaceSnapComponent } from './face-snap/face-snap.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [DatePipe],
+  providers: [{
+    provide: LOCALE_ID, useValue: 'fr-FR'
+  }, DatePipe],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  constructor() {
+    registerLocaleData(fr.default);
+  }
+}
